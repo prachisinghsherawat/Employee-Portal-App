e073658e680fab87ab3b806d0c46e52cbaf2eb58
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-08-19 16:41:14 +0530

    Register

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 import "./Register.css"
 
 export const Register = () => {
@@ -10,41 +11,75 @@ export const Register = () => {
 
     <div className="registerDiv">
 
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        '& > :not(style)': { m: 1 },
-      }}
-    >
+    <Box id="Box">
+
+
+        <div>
+        <label >Name : </label>
         <TextField
         id="demo-helper-text-misaligned"
         label="Name"
         />
+        </div>
 
+        <div>
+        <label >Email : </label>
         <TextField
         id="demo-helper-text-misaligned"
-        label="Name"
+        label="Email"
+        type="email"
         />
+        </div>
 
+        <div>
+        <label >Password : </label>
         <TextField
         id="demo-helper-text-misaligned"
-        label="Name"
+        label="Password"
+        type="password"
        />
+        </div>
 
+        <div>
+        <label >Username : </label>
         <TextField
         id="demo-helper-text-misaligned"
-        label="Name"
+        label="Username"
        />
+        </div>
 
+       <div>
+       <label >Mobile Number : </label>
         <TextField
         id="demo-helper-text-misaligned"
-        label="Name"
+        label="Mobile Number"
+        type="number"
        />
+       </div>
+
+        <div>
+        <label >Description : </label>
+        <TextField
+        id="demo-helper-text-misaligned"
+        label="Description"
+       />
+        </div>
+
+
+        <div id="btn">
+        <Button variant="contained">
+            Register Now
+        </Button>
+        </div>
 
       
     </Box>
 
+    
+
+
     </div>
   );
 }
+
+
